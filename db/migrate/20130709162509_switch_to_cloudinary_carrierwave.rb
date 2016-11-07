c97ac0ef6964be1ca1b82191e7681c33cfb7acb1
Matt Larson <larsonm@msu.edu>
2016-11-07 14:49:42 -0500

    Clone the app

--- db/migrate/20130709162509_switch_to_cloudinary_carrierwave.rb
+++ db/migrate/20130709162509_switch_to_cloudinary_carrierwave.rb
@@ -1,34 +1,34 @@
 class SwitchToCloudinaryCarrierwave < ActiveRecord::Migration
   def up
-    remove_column :profiles, :keep_file_file_name
-    remove_column :profiles, :keep_file_content_type
-    remove_column :profiles, :keep_file_file_size
-    remove_column :profiles, :keep_file_updated_at
-    
-    remove_column :profiles, :general_file_file_name
-    remove_column :profiles, :general_file_content_type
-    remove_column :profiles, :general_file_file_size
-    remove_column :profiles, :general_file_updated_at
-    
-    remove_column :profiles, :soldier_file_file_name
-    remove_column :profiles, :soldier_file_content_type
-    remove_column :profiles, :soldier_file_file_size
-    remove_column :profiles, :soldier_file_updated_at
-    
-    remove_column :profiles, :magic_file_file_name
-    remove_column :profiles, :magic_file_content_type
-    remove_column :profiles, :magic_file_file_size
-    remove_column :profiles, :magic_file_updated_at
-    
-    remove_column :profiles, :item_file_file_name
-    remove_column :profiles, :item_file_content_type
-    remove_column :profiles, :item_file_file_size
-    remove_column :profiles, :item_file_updated_at
-    
-    remove_column :profiles, :land_file_file_name
-    remove_column :profiles, :land_file_content_type
-    remove_column :profiles, :land_file_file_size
-    remove_column :profiles, :land_file_updated_at
+    # remove_column :profiles, :keep_file_file_name
+    # remove_column :profiles, :keep_file_content_type
+    # remove_column :profiles, :keep_file_file_size
+    # remove_column :profiles, :keep_file_updated_at
+    #
+    # remove_column :profiles, :general_file_file_name
+    # remove_column :profiles, :general_file_content_type
+    # remove_column :profiles, :general_file_file_size
+    # remove_column :profiles, :general_file_updated_at
+    #
+    # remove_column :profiles, :soldier_file_file_name
+    # remove_column :profiles, :soldier_file_content_type
+    # remove_column :profiles, :soldier_file_file_size
+    # remove_column :profiles, :soldier_file_updated_at
+    #
+    # remove_column :profiles, :magic_file_file_name
+    # remove_column :profiles, :magic_file_content_type
+    # remove_column :profiles, :magic_file_file_size
+    # remove_column :profiles, :magic_file_updated_at
+    #
+    # remove_column :profiles, :item_file_file_name
+    # remove_column :profiles, :item_file_content_type
+    # remove_column :profiles, :item_file_file_size
+    # remove_column :profiles, :item_file_updated_at
+    #
+    # remove_column :profiles, :land_file_file_name
+    # remove_column :profiles, :land_file_content_type
+    # remove_column :profiles, :land_file_file_size
+    # remove_column :profiles, :land_file_updated_at
     
     add_column :profiles, :keep, :string
     add_column :profiles, :general, :string
