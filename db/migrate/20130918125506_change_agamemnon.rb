527cc000b13c1546c6813eb575934bb3e06b60e7
Matt Larson <larsonm@msu.edu>
2016-11-07 15:25:36 -0500

    Clone the app

--- db/migrate/20130918125506_change_agamemnon.rb
+++ db/migrate/20130918125506_change_agamemnon.rb
@@ -1,6 +1,6 @@
 class ChangeAgamemnon < ActiveRecord::Migration
   def up
-    General.find_by_name('Agamemnon').update_attribute(:type,'Agamemnon')
+    # General.find_by_name('Agamemnon').update_attribute(:type,'Agamemnon')
   end
 
   def down
